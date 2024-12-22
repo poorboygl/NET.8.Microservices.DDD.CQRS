927886ed3b13fea13ef407c243cd65bd569ea7b0
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-12-22 07:37:16 +0700

    ch05-013 Develop Catalog.API Microservices Creating Domain Entities

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        List<string> Category { get; set; } = new();
+
+
+
+
+    }
+}
