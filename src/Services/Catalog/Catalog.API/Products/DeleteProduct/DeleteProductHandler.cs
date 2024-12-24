eb186399bbaa84933439640a01981f430ca748a6
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-12-24 10:56:48 +0700

    ch07-007 Develop CRUD Command Validators with Fluent Validation Pipeline Behaviour

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -6,6 +6,14 @@ namespace Catalog.API.Products.DeleteProduct
 {
     public record DeleteProductCommand(Guid Id): ICommand<DeleteProductResult>;
     public record DeleteProductResult(bool IsSuccess);
+    public class DeleteProductCommandValidator:  AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+        }
+    }
+
     internal class DeleteProductCommandHandler
         (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
         : ICommandHandler<DeleteProductCommand, DeleteProductResult>
