7cf10a5760aa46a892e75e5f074b174e22658293
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-12-25 10:51:31 +0700

    ch08-002 Create Asp.Net Web API for Basket.API Microservice in Microservices Solution

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// add services  to the container
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+// Configure the HTTP request pipeline
 
 app.Run();
