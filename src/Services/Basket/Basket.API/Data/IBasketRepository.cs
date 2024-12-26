595689b7ce3f99af1bc17fa0e8dea14cf09911bf
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-12-26 18:51:19 +0700

    ch09-004 Develop CachedBasketRepository with Proxy Pattern, Decorator Pattern and Scrutor

--- src/Services/Basket/Basket.API/Data/IBasketRepository.cs
+++ src/Services/Basket/Basket.API/Data/IBasketRepository.cs
@@ -2,7 +2,7 @@
 
 public interface IBasketRepository
 {
-    Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellation = default);
-    Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellation = default);
-    Task<bool> DeleteBasket(string userName, CancellationToken cancellation = default);
+    Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default);
+    Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default);
+    Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default);
 }
