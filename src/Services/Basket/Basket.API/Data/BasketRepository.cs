e14b144bf722066320c0403ac4cd0b75345357dd
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-12-26 13:27:40 +0700

    ch08-017 Develop BasketRepository Class using Marten Library

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Basket.API.Data
+{
+    public class BasketRepository : IBasketRepository
+    {
+        public Task<ShoppingCart> DeleteBasket(string userName, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellation = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
