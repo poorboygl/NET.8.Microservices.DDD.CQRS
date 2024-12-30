939f6e385f8d9f56781d26fd81b0c435aaaf773c
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-12-30 15:51:40 +0700

    ch11-012 Seed and Auto-Migrate SQLite Database using Entity Framework Core in Discount

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddGrpc();
 builder.Services.AddDbContext<DiscountContext>(opts => opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
-
+app.UseMigration();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
