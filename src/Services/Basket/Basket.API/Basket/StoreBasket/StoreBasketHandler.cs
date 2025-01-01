dfc56bbd77add649672f684703578cdf40131731
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-01-01 08:09:47 +0700

    ch12-003 Consuming Discount Grpc Service From Basket API Microservice When Adding Cart -2

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿using Discount.Grpc;
+
+namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart): ICommand<StoreBasketResult>;
 public record StoreBasketResult(string UserName);
@@ -11,17 +13,27 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     }
 }
 internal class StoreBasketCommandHandler
-    (IBasketRepository repository)
+    (IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto)
     : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
         //TODO:  comunicate  with Discount.Gprc  and calculate  lastest price  of product into sc
+        await DeductDiscount(command.Cart, cancellationToken);
 
-        ShoppingCart cart = command.Cart;
         //TODO: store basket in database (Use marten upsert- if exits = update
         await repository.StoreBasket(command.Cart, cancellationToken);
         //TODO: update cache
         return new StoreBasketResult(command.Cart.UserName);
+    }
+
+    private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        // Communicate with Discount.Grpc and calculate lastest prices of products into sc
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+            item.Price -= coupon.Amount;
+        }
     }
 }
