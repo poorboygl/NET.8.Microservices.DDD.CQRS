cf53765a3ca941d66942154271f95a808c8ec8e4
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-01-05 07:27:54 +0700

    ch14-002 Develop DDD Abstractions (Entity, Aggregate, DomainEvent) in Ordering.Domain

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions
+{
+    internal interface IDomainEvent : INotification
+    {
+        Guid EventId => Guid.NewGuid();
+        public string EventType => GetType().AssemblyQualifiedName;
+    }
+}
