9e5e7458954bf23f3054622ce0a9b546a8d97002
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-01-07 20:59:24 +0700

    ch14-009 Develop ValueObjects (Address, Payment) DDD Rich-domain model Entity

--- src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -9,4 +9,30 @@ public record Address
     public string Country { get; } = default!;
     public string State { get; } = default!;
     public string ZipCode { get; } = default!;
+
+    protected Address()
+    {
+
+    }
+
+    private Address(string firstName, string lastName,string emailAddress, 
+                    string addressLine, string country, string state, string zipcode)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        EmailAddress = emailAddress;
+        AddressLine = addressLine;
+        Country = country;
+        State = state;
+        ZipCode = zipcode;
+    }
+
+    public static Address Of(string firstName, string lastName, string emailAddress,
+                    string addressLine, string country, string state, string zipcode)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(emailAddress);
+        ArgumentException.ThrowIfNullOrWhiteSpace(addressLine);
+
+        return new Address(firstName,lastName,emailAddress,addressLine, country, state, zipcode);
+    }
 }
--- src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -1,11 +1,11 @@
 ﻿namespace Ordering.Domain.ValueObjects;
 
-public class CustomerId
+public record CustomerId
 {
     public Guid Value { get; }
     private CustomerId(Guid value) => Value = value;
 
-    public static CustomerId Off(Guid value)
+    public static CustomerId Of(Guid value)
     {
         ArgumentNullException.ThrowIfNull(value);
         if (value == Guid.Empty)
