4c0149408ccdec2bf7adc9fb7aec84f96077d1cb
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-01-07 11:39:21 +0700

    ch14-008 Develop DDD Rich-domain model Entity for Order Entities in Order.Domain Layer

---
+++ src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Domain.Exceptions;
+
+public class DomainException : Exception
+{
+    public DomainException(string messsage) : base($"Domain exception: \"{messsage}\" throw from Domain Layer. ")
+    {
+        
+    }
+}
--- src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -3,4 +3,16 @@
 public class CustomerId
 {
     public Guid Value { get; }
+    private CustomerId(Guid value) => Value = value;
+
+    public static CustomerId Off(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+        {
+            throw new DomainException("CustomerId cannot be empty.");
+        }
+
+        return new CustomerId(value);
+    }
 }
