cd7f9a3917f58d6d4f4cb4a433ccf8671b177546
nguyenvu-laptop <nguye@LAPTOP>
2025-02-19 15:00:45 +0700

    ch18-023 Develop Basket Checkout Event Consumer integration event handler class

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+using Ordering.Application.Orders.Commands.CreateOrder;
+
+namespace Ordering.Application.Orders.EventHandlers.Integration;
+public class BasketCheckoutEventHandler
+    : IConsumer<BasketCheckoutEvent>
+{
+    public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    {
+        // TODO: Create new order and start order fullfillment process
+        throw new NotImplementedException();
+    }
+}
