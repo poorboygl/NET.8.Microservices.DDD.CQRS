266cb3871cd4d3d1ffb7e8aa5019a813bf540d7e
nguyenvu-laptop <nguye@LAPTOP>
2025-02-27 01:16:02 +0700

    ch20-018 AddToCart Submit Handle in Index cshtml Page with IBasketService

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,18 +1,38 @@
-namespace Shopping.Web.Pages
-{
-    public class IndexModel(ICatalogService catalogService, ILogger<IndexModel> logger)
+namespace Shopping.Web.Pages;
+public class IndexModel
+    (ICatalogService catalogService, IBasketService basketService, ILogger<IndexModel> logger)
     : PageModel
+{
+    public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();
 
+    public async Task<IActionResult> OnGetAsync()
     {
-        public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();
+        logger.LogInformation("Index page visited");
+        var result = await catalogService.GetProducts();
+        //var result = await catalogService.GetProducts(2, 3);
+        ProductList = result.Products;
+        return Page();
+    }
 
-        public async Task<IActionResult> OnGetAsync()
+    public async Task<IActionResult> OnPostAddToCartAsync(Guid productId)
+    {
+        logger.LogInformation("Add to cart button clicked");
+
+        var productResponse = await catalogService.GetProduct(productId);
+
+        var basket = await basketService.LoadUserBasket();
+
+        basket.Items.Add(new ShoppingCartItemModel
         {
-            logger.LogInformation("Index page visited");
-            var result = await catalogService.GetProducts();
-            //var result = await catalogService.GetProducts(2, 3);
-            ProductList = result.Products;
-            return Page();
-        }
+            ProductId = productId,
+            ProductName = productResponse.Product.Name,
+            Price = productResponse.Product.Price,
+            Quantity = 1,
+            Color = "Black"
+        });
+
+        await basketService.StoreBasket(new StoreBasketRequest(basket));
+
+        return RedirectToPage("Cart");
     }
 }
--- src/WebApps/Shopping.Web/Services/IBasketService.cs
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -17,4 +17,27 @@ public interface IBasketService
     [Post("/basket-service/basket/checkout")]
     Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
 
+    public async Task<ShoppingCartModel> LoadUserBasket()
+    {
+        // Get Basket If Not Exist Create New Basket with Default Logged In User Name: swn
+        var userName = "swn";
+        ShoppingCartModel basket;
+
+        try
+        {
+            var getBasketResponse = await GetBasket(userName);
+            basket = getBasketResponse.Cart;
+        }
+        catch (ApiException apiException) when (apiException.StatusCode == HttpStatusCode.NotFound)
+        {
+            basket = new ShoppingCartModel
+            {
+                UserName = userName,
+                Items = []
+            };
+        }
+
+        return basket;
+    }
+
 }
