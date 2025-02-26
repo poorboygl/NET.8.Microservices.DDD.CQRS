47a4c13d055ab75fcba0a00231cae64f932a17eb
nguyenvu-laptop <nguye@LAPTOP>
2025-02-27 00:30:47 +0700

    ch20-011 Install and Configure Refit HttpClientFactory Library in our Shopping.Web ms

--- src/WebApps/Shopping.Web/GlobalUsing.cs
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -1,3 +1,5 @@
-﻿global using Microsoft.AspNetCore.Mvc;
+﻿global using Shopping.Web.Models.Catalog;
+global using Refit;
+global using Shopping.Web.Services;
+global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Mvc.RazorPages;
-global using Shopping.Web.Models.Catalog;
