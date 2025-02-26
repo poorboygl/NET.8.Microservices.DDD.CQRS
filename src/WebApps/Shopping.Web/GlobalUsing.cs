0653f632866d813e1e95488d6fae28541fd28477
nguyenvu-laptop <nguye@LAPTOP>
2025-02-27 00:37:20 +0700

    ch20-014 Develop IOrderingService Classes with Refit HttpClientFactory Library

--- src/WebApps/Shopping.Web/GlobalUsing.cs
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -1,4 +1,6 @@
 ﻿global using Shopping.Web.Models.Catalog;
+global using Shopping.Web.Models.Basket;
+global using Shopping.Web.Models.Ordering;
 global using Refit;
 global using Shopping.Web.Services;
 global using Microsoft.AspNetCore.Mvc;
