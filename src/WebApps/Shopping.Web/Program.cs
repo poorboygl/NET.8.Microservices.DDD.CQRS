5d8ac0543003f3c004954e5d7b739b9bb93779d6
nguyenvu-laptop <nguye@LAPTOP>
2025-02-28 01:40:00 +0700

    ch02-011 Adding LoggingDelegatingHandler for Intercepting Microservices Request-Response

--- src/WebApps/Shopping.Web/Program.cs
+++ src/WebApps/Shopping.Web/Program.cs
@@ -15,24 +15,25 @@ try
 
     // Add services to the container
     builder.Services.AddRazorPages();
+    builder.Services.AddTransient<LoggingDelegatingHandler>();
 
     builder.Services.AddRefitClient<ICatalogService>()
         .ConfigureHttpClient(c =>
         {
             c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
-        });
+        }).AddHttpMessageHandler<LoggingDelegatingHandler>();
 
     builder.Services.AddRefitClient<IBasketService>()
         .ConfigureHttpClient(c =>
         {
             c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
-        });
+        }).AddHttpMessageHandler<LoggingDelegatingHandler>();
 
     builder.Services.AddRefitClient<IOrderingService>()
         .ConfigureHttpClient(c =>
         {
             c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
-        });
+        }).AddHttpMessageHandler<LoggingDelegatingHandler>();
 
     var app = builder.Build();
 
